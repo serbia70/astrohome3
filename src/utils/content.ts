6a3c0802c62cc1b999eee05841e2094763e59bf5
serbia70 <serbia1688@gmail.com>
2025-02-23 11:37:12 +0100

    Sync at 2025-02-23 周日 11:37:11.81 by chenan

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -2,25 +2,25 @@ import { categories } from '@data/category';
 import { getCollection } from 'astro:content';
 
 // 如果需要接入 Notion 数据源，需要将下面的注释去掉
-// export const getNotionDocs = (await getCollection('notion')).map((item) => {
-//   return {
-//     id: item.id,
-//     body: item.rendered?.html ?? '',
-//     data: {
-//       title: richTextToPlainText(item.data.properties.title.rich_text),
-//       desc: richTextToPlainText(item.data.properties.desc.rich_text),
-//       category: item.data.properties.category.select?.name || '',
-//       author: richTextToPlainText(item.data.properties.author.rich_text),
-//       tags: item.data.properties.tags.rich_text[0].plain_text.split(','),
-//       image: item.data.properties.image.url,
-//       publishDate: item.data.properties.publishDate.created_time,
-//       pin: false
-//     },
-//     rendered: item.rendered,
-//     filePath: item.id,
-//     collection: item.collection
-//   };
-// });
+ export const getNotionDocs = (await getCollection('notion')).map((item) => {
+   return {
+     id: item.id,
+     body: item.rendered?.html ?? '',
+     data: {
+       title: richTextToPlainText(item.data.properties.title.rich_text),
+       desc: richTextToPlainText(item.data.properties.desc.rich_text),
+       category: item.data.properties.category.select?.name || '',
+       author: richTextToPlainText(item.data.properties.author.rich_text),
+       tags: item.data.properties.tags.rich_text[0].plain_text.split(','),
+       image: item.data.properties.image.url,
+       publishDate: item.data.properties.publishDate.created_time,
+       pin: false
+     },
+     rendered: item.rendered,
+     filePath: item.id,
+     collection: item.collection
+   };
+ });
 
 // Only return posts without `draft: true` in the frontmatter
 export const latestPosts = [
@@ -28,7 +28,7 @@ export const latestPosts = [
     return import.meta.env.PROD ? data.draft !== true : true;
   }))
   // 如果需要接入 Notion 数据源，需要将下面的注释去掉
-  // ...getNotionDocs
+   ...getNotionDocs
 ].sort((a, b) => new Date(b.data.publishDate).valueOf() - new Date(a.data.publishDate).valueOf());
 
 /**
